2dcf0fe57b9bfaa1a48db0f277ffdce363392a4a
nicefaa6waa <mammadli0088@outlook.com>
2025-03-14 16:24:28 +0300

    sha256 added

--- UpdateUsers.sh
+++ UpdateUsers.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 NAME="genshin-sddm-theme"
-DIR="/usr/share/sddm/themes/$NAME/"
-CFG="/etc/sddm.conf"
+DIR="/usr/share/sddm/themes/$NAME/components/"
 
 function displayArtAndWelcome {
 
@@ -65,8 +64,6 @@ function selectOS {
     done
 }
 
-
-
 function installPackages {
     case $1 in
         Ubuntu )
@@ -82,100 +79,6 @@ function installPackages {
             echo "Error: Invalid OS option"
             ;;
     esac
-}
-
-function updateXsetup {
-    XSETUP_FILE="/usr/share/sddm/scripts/Xsetup"
-
-    echo -e "\nYour resolution is too high. Do you want to add an xrandr configuration for better compatibility?"
-    select sel in "Yes" "No"; do
-        case $sel in
-            Yes )
-                echo "Enter your desired resolution (e.g., 1920x1080):"
-                read -r desired_resolution
-
-                XRANDR_CMD="xrandr --output eDP-1 --mode $desired_resolution --pos 0x0 --rotate normal"
-
-                if ! sudo grep -Fxq "$XRANDR_CMD" "$XSETUP_FILE"; then
-                    echo "$XRANDR_CMD" | sudo tee -a "$XSETUP_FILE" > /dev/null
-                    echo "Xsetup file updated with xrandr command for resolution $desired_resolution."
-                else
-                    echo "xrandr command for resolution $desired_resolution already exists in Xsetup file."
-                fi
-                break;;
-            No )
-                echo "No changes made to the Xsetup file."
-                break;;
-            * )
-                echo "Invalid option. Please select 1 for Yes or 2 for No."
-                ;;
-        esac
-    done
-}
-
-
-
-function changeCurrentTheme {
-    sudo sed -i "s/^Current=.*/Current=$NAME/" $CFG
-    echo "Current theme changed to $NAME"
-}
-
-function disableVirtualKeyboard {
-    if ! grep -wq "InputMethod=" $CFG; then
-        echo -e "\nDo you want to disable the virtual on-screen keyboard in SDDM? Select yes if you have a physical keyboard"
-        select sel in "Yes" "No"; do
-            case $sel in
-                Yes ) 
-                if grep -q "^InputMethod=qtvirtualkeyboard" $CFG; then
-                    sudo sed -i "s/^InputMethod=qtvirtualkeyboard/InputMethod=/" $CFG;
-                    echo "Virtual keyboard disabled (modified InputMethod entry)";
-                elif ! grep -q "^InputMethod=" $CFG; then
-                    sudo sed -i 's/^\[General\]/\[General\]\nInputMethod=/' $CFG;
-                    echo "Virtual keyboard disabled (created empty InputMethod entry)";
-                fi
-                break;;
-                No ) break;;
-            esac
-        done
-    fi
-}
-
-function download_from_mega {
-    echo "Changing directory"
-    cd "backgrounds"
-    echo "Downloading videos from Mega.nz..."
-    megadl 'https://mega.nz/file/AqNS0Sxa#3_E0apJ6JFmIFsGJ6_q1XXlI0klsXwsnh3QrRZhe6nI' 
-    megadl 'https://mega.nz/file/tnMWiTaa#0KRYpof6fJ0cF1m85N1ZF-7AKaqDm2GaaZYWo8D3P70' 
-    megadl 'https://mega.nz/file/cj8CCY6J#Ap94bhUgEocvMF9EsX_tQCnfF0hbOxA4JY3POtArLDk'
-	echo "Changing Directory Back"
-    cd ..
-}
-
-# Function to download videos from Dropbox
-function download_from_dropbox {
-    echo "Changing directory"
-    cd "backgrounds"
-    echo "Downloading videos from Dropbox..."
-    curl -L 'https://www.dropbox.com/scl/fi/e6kio0zet8j5lax9ztu7y/nightbg.mp4?rlkey=29ni1jq0a9sr02b6l0s61oiiq&st=en7rqgmy&dl=0' > nightbg.mp4 
-    curl -L 'https://www.dropbox.com/scl/fi/8om5p5d3ul984krbnhfgu/morningbg.mp4?rlkey=jedf5qcycj310oqx8djy9yomz&st=yupg7zz8&dl=0' > morningbg.mp4 
-    curl -L 'https://www.dropbox.com/scl/fi/f3x5jfr8sk06ij3c8kgoy/sunrisebg.mp4?rlkey=r0lzylvhze4lq3xglwkvf4iya&st=ybukfsgv&dl=0' > sunrisebg.mp4
-	echo "Changing Directory Back"
-    cd ..
-}
-
-function choose_server {
-    echo "Choose the server to download videos from:"
-    echo "1) Mega.nz"
-    echo "2) Dropbox"
-    read -p "Enter the number (1 or 2): " server_choice
-    if [ "$server_choice" == "1" ]; then
-        download_from_mega || download_from_dropbox
-    elif [ "$server_choice" == "2" ]; then
-        download_from_dropbox || download_from_mega
-    else
-        echo "Invalid choice. Defaulting to Mega.nz."
-        download_from_mega || download_from_dropbox
-    fi
 }
 
 function handleMultipleAccounts {
@@ -196,93 +99,8 @@ function handleMultipleAccounts {
     done
 }
 
-function testTheme {
-    echo -e "\nDo you want to test the theme now?"
-    select sel in "Yes" "No"; do
-        case $sel in
-            Yes ) sddm-greeter --test-mode --theme $DIR; break;;
-            No ) exit;;
-        esac
-    done
-}
-
-function createConfig {
-    sddm --example-config | sudo tee $CFG > /dev/null
-    echo "Configuration file created in $CFG"
-}
-
-function skipLoadingAnimation {
-  echo -e "\nDo you want to skip the login screen loading animation?"
-  select sel in "Yes" "No"; do
-    case $sel in
-      Yes )
-        echo "Loading animation will be skipped."
-        if [ -f "components/LoginPanel.qml" ]; then
-          rm "components/LoginPanel.qml"
-        fi
-
-        if [ -f "components/LoginPanel2.qml" ]; then
-          mv "components/LoginPanel2.qml" "components/LoginPanel.qml"
-        else
-          echo "LoginPanel2.qml not found. Skipping rename."
-        fi
-        ;;
-      No )
-        echo "Loading animation will be displayed."
-        if [ -f "components/LoginPanel2.qml" ]; then
-          rm "components/LoginPanel2.qml"
-        fi
-        ;;
-      * )
-        echo "Invalid selection. Please try again."
-        ;;
-    esac
-    break
-  done
-}
-
-
-
-function mainOperations {
-    if [ ! -f $CFG ]; then
-        echo -e "\nSDDM configuration file $CFG does not exist. Do you want to create it based on current configuration?"
-        select sel in "Yes" "No"; do
-            case $sel in
-                Yes )
-                    createConfig
-		    updateXsetup
-                    changeCurrentTheme
-                    selectOS
-                    choose_server
-                    skipLoadingAnimation
-                    sudo cp -R . $DIR
-                    disableVirtualKeyboard
-                    testTheme
-                    break
-                    ;;
-                No )
-                    echo "Theme will be installed in $DIR but configuration not changed."
-                    choose_server
-		    updateXsetup
-                    skipLoadingAnimation
-                    sudo cp -R . $DIR
-                    testTheme
-                    break
-                    ;;
-            esac
-        done
-    else
-        selectOS
-        choose_server
-        updateXsetup
-        skipLoadingAnimation
-        sudo cp -R . $DIR
-        changeCurrentTheme
-        disableVirtualKeyboard
-        testTheme
-    fi
-}
-
-displayArtAndWelcome    
+displayArtAndWelcome
+selectOS
+installPackages
 handleMultipleAccounts
-mainOperations
+sudo cp /components/credentials.txt $DIR
